b03267be8f836997ec257ddc38d16d6053a82386
Daniele Minieri <danieleminieri99@gmail.com>
2023-02-21 17:14:12 +0100

    Milestone 2 completata

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
 <?php 
-// Funzione che genera la password
-function createPsw($charsnum, $huge_array){
-    $repetition = $_GET['repetition'];
-    if($repetition == 'false'){
-        $repetition = false;
-        if($charsnum > 10 && count($huge_array) == 10){
-        $charsnum = 10;
-        }
-        $my_temp_array = [];
-        while(count($my_temp_array) <= $charsnum){
-        $my_var=array_rand(array_flip($huge_array), 1);
-        if(!in_array($my_var, $my_temp_array)){
-            $my_temp_array[] = $my_var;
-        }
-    }$password=implode($my_temp_array);
-    } else{
-        $password=(str_shuffle(implode(array_rand(array_flip($huge_array), $charsnum))));
-    }
-    return $password;  
-}
+require_once __DIR__ . '/functions.php';
 
 $charsnum = intval($_GET['charsnumber'] ?? '');
 
